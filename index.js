561e6ac84aef5c8d77948593374ca3a6f2993031
HakeemAlavi <hakeem.alavi@strathmore.edu>
2024-04-16 18:05:36 +0300

    feat: modify Index script

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// Import the express module
+const express = require('express');
+// Create an instance of the express application
+const app = express();
+// Import the catsRouter module
+const catsRouter = require('./routes/cats.router');
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Mount the catsRouter at the '/cats' endpoint
+app.use('/cats', catsRouter);
+
+// Route handler for the root endpoint
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  // Send 'Hello World!' as the response
+  res.send('Hello World!');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// Start the server
+const PORT = process.env.PORT || 3000; // Define the port number
+app.listen(PORT, () => {
+  // Callback function executed when the server starts
+  console.log(`Server is running on port ${PORT}`);
+});
